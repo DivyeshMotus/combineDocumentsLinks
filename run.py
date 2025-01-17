65c64edf78a964faec5111eb96ce3c2532e15bc1
DivyeshMotus <divyesh.ved@motusnova.com>
2025-01-17 03:05:01 +0000

    Fixed all errors

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ from PyPDF2 import PdfMerger
 load_dotenv()
 
 FOLDER_ID = os.getenv('FOLDER_ID')
+SCOPES = ['https://www.googleapis.com/auth/drive']
 
 def authenticate_services():
     credentials = service_account.Credentials.from_service_account_file('credentials.json', scopes=SCOPES)
@@ -18,24 +19,20 @@ def authenticate_services():
 def enter_links():
     links = []
     for i in range(7):
-        input_link = input(f"Enter Link {i+1}: ")
-        if input_link != "":
-            links.append(str(input_link))
-        else:
-            links.append("-")
+        input_link = input(f"Enter Link {i+1}: ").strip()
+        if input_link:
+            links.append(input_link)
     return links
 
 def create_folder():
-    folder_path = f"downloads"
+    folder_path = "downloads"
     os.makedirs(folder_path, exist_ok=True)
     os.chmod(folder_path, 0o777)
     return folder_path
 
-def download_google_drive_file(link, output_path):
+def download_files(links, output_path):
     file_counter = 1
     for link in links:
-        if link == "-":
-            cotinue
         if "drive.google.com" in link:
             file_id = link.split("/d/")[1].split("/")[0]
             download_url = f"https://drive.google.com/uc?id={file_id}"
@@ -49,10 +46,10 @@ def merge_pdfs_in_folder(folder_path):
     pdf_order = ['1', '2', '3', '4', '5', '6', '7']
     for pdf_type in pdf_order:
         for item in os.listdir(folder_path):
-            if item.endswith('.pdf') and pdf_type.lower() in item.lower():
+            if item.endswith('.pdf') and pdf_type in item:
                 pdf_path = os.path.join(folder_path, item)
                 merger.append(pdf_path)
-    merged_pdf_path = os.path.join(folder_path, f'merged_document.pdf')
+    merged_pdf_path = os.path.join(folder_path, 'merged_document.pdf')
     merger.write(merged_pdf_path)
     merger.close()
     return merged_pdf_path
@@ -72,10 +69,10 @@ def main():
     links = enter_links()
     drive_service = authenticate_services()
     folder_path = create_folder()
-    file_paths = download_files(links, folder_path)
+    download_files(links, folder_path)
     merged_pdf_path = merge_pdfs_in_folder(folder_path)
-    shareable_link = upload_file_to_drive(drive_service, merged_pdf_path, "combinedDoc.pdf", )
-    print(F"Final Shareable Link: {shareable_link}")
+    shareable_link = upload_file_to_drive(drive_service, merged_pdf_path, "combinedDoc.pdf", FOLDER_ID)
+    print(f"Final Shareable Link: {shareable_link}")
 
 if __name__ == '__main__':
     main()
